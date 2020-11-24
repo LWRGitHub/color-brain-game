b77f3394ad2b917fb135dfa8693ad98b577e1cbf
LWRGitHub <LoganReynoldsLWR@gmail.com>
2020-11-23 17:14:28 -0800

    Initial Commit

---
+++ Brain-game/Brain_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brain_gameApp.swift
+//  Brain-game
+//
+//  Created by Logan Reynolds on 11/23/20.
+//
+
+import SwiftUI
+
+@main
+struct Brain_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
