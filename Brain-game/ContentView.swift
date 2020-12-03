91ec751dfaeb1d30af51b7b4f40d149ad1c99bf4
LWRGitHub <LoganReynoldsLWR@gmail.com>
2020-12-02 19:39:10 -0800

    first commit

--- Brain-game/ContentView.swift
+++ Brain-game/ContentView.swift
@@ -7,11 +7,125 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+enum ColorOption{
+    case red, orange, yellow, green , blue, purple
+    
+    var textColor: Color {
+        switch self {
+        case .red:
+            return .red
+        case .orange:
+            return .orange
+        case .yellow:
+            return .yellow
+        case .green:
+            return .green
+        case .blue:
+            return .blue
+        case .purple:
+            return .purple
+        }
     }
+    
+    var text: String {
+        switch self {
+        case .red:
+            return "red"
+        case .orange:
+            return "orange"
+        case .yellow:
+            return "yellow"
+        case .green:
+            return "green"
+        case .blue:
+            return "blue"
+        case .purple:
+            return "purple"
+        }
+    }
+    
+    init() {
+        self = ColorOption.allCases[Int(arc4random_uniform(UInt32(ColorOption.allCases.count))())]
+    }
+}
+
+extension ColorOption: CaseIterable {
+    mutating func getRandomColor(){
+        self = ColorOption.allCases[Int(arc4random_uniform(UInt32(ColorOption.allCases.count))())]
+    }
+}
+
+struct ContentView: View {
+    
+    @State var upperColor = ColorOption()
+    @State var bottomColor = ColorOption()
+    @State var displayColor = ColorOption()
+    
+    @State var answer: Bool = false
+    @State var score: Int = 0
+    
+    var body: some View {
+        VStack{
+            Text("Score: \(score)")
+            Text("Does the meaning match the text color?")
+                .padding()
+            VStack{
+                Text("meaning")
+                    .font(.caption)
+                Text("\(upperColor.text)")
+                    .font(.largeTitle)
+            }.padding()
+            VStack{
+                Text("\(displayColor.text)")
+                    .font(.largeTitle)
+                    .foregroundColor(bottomColor.textColor)
+                Text("text color")
+                    .font(.caption)
+            }.padding()
+            HStack{
+                Group{
+                    Button(action: {
+                        answer = false
+                        checkAnswer()
+                    }) {
+                        Text("No")
+                            .padding()
+                            .foregroundColor(.white)
+                    }
+                }
+                .background(Color(.white))
+                .padding()
+                Group{
+                    Button(action: {
+                        answer = true
+                        checkAnswer()
+                    }) {
+                        Text("Yes")
+                            .padding()
+                            .foregroundColor(.white)
+                    }
+                }
+                .background(Color(.white))
+                .padding()
+            }.padding()
+        }
+    }
+    
+    func checkAnswer(){
+        if upperColor == bottomColor && answer{
+            score += 1
+        } else if upperColor != bottomColor && !answer{
+            score += 1
+        }
+        reloadColors()
+    }
+    
+    func reloadColors(){
+        upperColor = ColorOption()
+        bottomColor = ColorOption()
+        displayColor = ColorOption()
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ README.md
@@ -0,0 +1,18 @@
+# Brain Game
+
+
+## How To Play:
+- The color of the 2nd word will change 
+- When the spelling of the 1st word matches the color of the 2nd word press "YES"
+- When the spelling of the 1st word Does NOT match the color of the 2nd word press "NO"
+
+## purpose of game:
+- This kind of game has been said to strengthen the mind and make people smarter
+- it's alos fun!
+
+## Tools/Languages
+- Swift
+- Xcode
+- SwiftUI
+
+
